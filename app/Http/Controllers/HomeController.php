59a812baf46250cb4338910893a57774e13defd6
ardilalukita14 <lukitasari623@gmail.com>
2021-05-29 13:41:00 +0700

    Membuat HomeController  untuk menghubungkan view dan model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    /**public function __construct()
     {
         $this->middleware('auth');
     }
@@ -23,6 +23,156 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('index');
+    }
+
+    public function cart()
+    {
+        return view('cart');
+    }
+
+    /**public function checkout()
+    {
+        return view('checkout');
+    }
+*/
+    public function shop()
+    {
+        return view('shop');
+    }
+
+    public function checkout()
+    {
+        return view('checkout');
+    }
+
+    public function pricing()
+    {
+        return view('pricing');
+    }
+
+    public function confirmation()
+    {
+        return view('confirmation');
+    }
+
+    public function productSingle()
+    {
+        return view('productSingle');
+    }
+
+    public function shopSidebar()
+    {
+        return view('shopSidebar');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    /**public function contact()
+    {
+        return view('contact');
+    }
+*/
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function notFound()
+    {
+        return view('404');
+    }
+
+    public function comingSoon()
+    {
+        return view('comingSoon');
+    }
+
+    public function faq()
+    {
+        return view('faq');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function signin()
+    {
+        return view('signin');
+    }
+
+    /**public function forgetPassword()
+    {
+        return view('forgetPassword');
+    }
+*/
+    public function forgetPassword()
+    {
+        return view('forgetPassword');
+    }
+
+    public function blogLeftSidebar()
+    {
+        return view('blogLeftSidebar');
+    }
+
+    public function blogRightSidebar()
+    {
+        return view('blogRightSidebar');
+    }
+
+    public function blogFullWidth()
+    {
+        return view('blogFullWidth');
+    }
+
+    public function blogGrid()
+    {
+        return view('blogGrid');
+    }
+
+    public function blogSingle()
+    {
+        return view('blogSingle');
+    }
+
+    public function typography()
+    {
+        return view('typography');
+    }
+
+    public function buttons()
+    {
+        return view('buttons');
+    }
+
+    public function alerts()
+    {
+        return view('alerts');
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function order()
+    {
+        return view('order');
+    }
+
+    public function address()
+    {
+        return view('address');
+    }
+
+    public function profileDetails()
+    {
+        return view('profileDetails');
     }
 }
