315cf76d0e24461278130cc1b4ca0ad7e6b93eda
ardilalukita14 <lukitasari623@gmail.com>
2021-05-30 18:46:58 +0700

    Membuat HomeController untuk menghubungkan view dan model

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
      *
      * @return void
      */
-    /**public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
     }
@@ -25,114 +25,6 @@ class HomeController extends Controller
     {
         return view('index');
     }
-
-    public function cart()
-    {
-        return view('cart');
-    }
-
-    /**public function checkout()
-    {
-        return view('checkout');
-    }
-*/
-    public function shop()
-    {
-        return view('shop');
-    }
-
-    public function checkout()
-    {
-        return view('checkout');
-    }
-
-    public function pricing()
-    {
-        return view('pricing');
-    }
-
-    public function confirmation()
-    {
-        return view('confirmation');
-    }
-
-    public function productSingle()
-    {
-        return view('productSingle');
-    }
-
-    public function shopSidebar()
-    {
-        return view('shopSidebar');
-    }
-
-    public function contact()
-    {
-        return view('contact');
-    }
-
-    /**public function contact()
-    {
-        return view('contact');
-    }
-*/
-    public function about()
-    {
-        return view('about');
-    }
-
-    public function notFound()
-    {
-        return view('404');
-    }
-
-    public function comingSoon()
-    {
-        return view('comingSoon');
-    }
-
-    public function faq()
-    {
-        return view('faq');
-    }
-
-    public function login()
-    {
-        return view('login');
-    }
-
-    public function signin()
-    {
-        return view('signin');
-    }
-
-    /**public function forgetPassword()
-    {
-        return view('forgetPassword');
-    }
-*/
-    public function forgetPassword()
-    {
-        return view('forgetPassword');
-    }
-
-    public function dashboard()
-    {
-        return view('dashboard');
-    }
-
-    public function order()
-    {
-        return view('order');
-    }
-
-    public function address()
-    {
-        return view('address');
-    }
-
-    public function profileDetails()
-    {
-        return view('profileDetails');
-    }
 }
+
+   
