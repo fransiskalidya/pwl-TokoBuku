b1ad2733f06518e0c7d021fa307b2c1349a12184
ardilalukita14 <lukitasari623@gmail.com>
2021-05-30 18:46:44 +0700

    Membuat BukuController untuk menghubungkan view dan model

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BukuController extends Controller
+{
+
+    public function cart()
+    {
+        return view('cart');
+    }
+
+    /**public function checkout()
+    {
+        return view('checkout');
+    }
+*/
+    public function shop()
+    {
+        return view('shop');
+    }
+
+    public function checkout()
+    {
+        return view('checkout');
+    }
+
+
+    public function productSingle()
+    {
+        return view('productSingle');
+    }
+
+    public function shopSidebar()
+    {
+        return view('shopSidebar');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    /**public function contact()
+    {
+        return view('contact');
+    }
+*/
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function login()
+    {
+        return view('login');
+    }
+
+    public function signin()
+    {
+        return view('signin');
+    }
+
+    /**public function forgetPassword()
+    {
+        return view('forgetPassword');
+    }
+*/
+    public function order()
+    {
+        return view('order');
+    }
+}
