bae8e4b2795e715df5d688ccc54924aaf4afcfd4
ardilalukita14 <lukitasari623@gmail.com>
2021-05-30 12:48:55 +0700

    Membuat view 404 untuk menunjukkan bahwa "Page Not Found"

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -20,7 +20,7 @@ FACEBOOK: https://www.facebook.com/themefisher
   <!-- Basic Page Needs
   ================================================== -->
   <meta charset="utf-8">
-  <title>Aviato | E-commerce template</title>
+  <title>Erfolg Store | Book Store</title>
 
   <!-- Mobile Specific Metas
   ================================================== -->
@@ -59,7 +59,7 @@ FACEBOOK: https://www.facebook.com/themefisher
 					</a>
 					<h1>404</h1>
 					<h2>Page Not Found</h2>
-					<a href="index.html" class="btn btn-main"><i class="tf-ion-android-arrow-back"></i> Go Home</a>
+					<a href="{{ url('/') }}" class="btn btn-main"><i class="tf-ion-android-arrow-back"></i> Go Home</a>
 					<p class="copyright-text">© 2018 Themefisher All Rights Reserved</p>
 				</div>
 			</div>
