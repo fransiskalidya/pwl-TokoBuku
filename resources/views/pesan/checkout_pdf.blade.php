6cfa99812a56eed21493115d393e8b3acb1e45a3
ardilalukita14 <lukitasari623@gmail.com>
2021-06-06 12:14:20 +0700

    Membuat menu cetak pdf checkout

--- resources/views/pesan/checkout_pdf.blade.php
+++ resources/views/pesan/checkout_pdf.blade.php
@@ -29,21 +29,67 @@
                 <p><strong>Email :</strong> {{ $Checkout1->id_user->email }}</p>
             </div>
     <br><br><br><br><br><br>
-    <table class="table table-bordered">
-        <tr>
-            <th>Mata Kuliah</th>
-            <th>SKS</th>
-            <th>Semester</th>
-            <th>Nilai</th>
-        </tr>
-        @foreach ($Mahasiswa->matakuliah as $matkul)
-        <tr>  
-            <td>{{ $matkul->nama_matkul }}</td>
-            <td>{{ $matkul->sks }}</td>
-            <td>{{ $matkul->semester }}</td>
-            <td>{{ $matkul->pivot->nilai }}</td>
-        </tr>
-        @endforeach
+    <div class="col-md-12">
+            <div class="card">
+            <div class="card-body">
+                    <h3><i class="tf-ion-android-cart"></i> Check Out</h3>
+                    @if(!empty($pesanan))
+                    <p align="right">Tanggal Pesan : {{ $pesanan->tanggal }}</p>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Gambar</th>
+                                <th>Judul Buku</th>
+                                <th>Jumlah</th>
+                                <th>Harga</th>
+                                <th>Total Harga</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no = 1; ?>
+                            @foreach($pesanan_details as $pesanan_detail)
+                            <tr>
+                                <td align="center">{{ $no++ }}</td>
+                                <td align="center">
+                                    <img src="{{ url('Novel') }}/{{ $pesanan_detail->buku->image }}" width="100" alt="...">
+                                </td>
+                                <td align="center">{{ $pesanan_detail->buku->nama_buku }}</td>                              
+                                <td align="center">{{ $pesanan_detail->jumlah }} buku</td>
+                                <td align="center">Rp. {{ number_format($pesanan_detail->buku->harga) }}</td>
+                                <td align="center">Rp. {{ number_format($pesanan_detail->subtotal) }}</td>
+                                <td align="center">
+                                    <form action="{{ url('check-out') }}/{{ $pesanan_detail->id }}" method="post">
+                                        @csrf
+                                        {{ method_field('DELETE') }}
+                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Anda yakin akan menghapus data ?');">
+                                        <i class="tf-ion-android-delete"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <td colspan="5" align="right"><strong>Total Harga :</strong></td>
+                                <td align="center"><strong>Rp. {{ number_format($pesanan->total) }}</strong></td>
+                                <td align="center">
+                                    <a href="{{ url('konfirmasi-check-out') }}" class="btn btn-success" onclick="return confirm('Anda yakin akan Check Out ?');">
+                                        <i class="tf-ion-android-cart"></i> Check Out
+                                    </a>
+                                </td>
+                                </table>
+                            <div class="float-right my-2">
+                                <a class="btn btn-success mt-3" href="{{ route('pesan.index') }}">Kembali</a>
+                            </div>
+                            <div class="float-left my-2 text-center">
+                                <a class="btn btn-danger mt-3" href="{{ route('mahasiswa.pdf', $Mahasiswa->Nim) }}">Cetak ke PDF</a>
+                            </div>
+                        </div>
+                    </div>
+                            </tr>
+                        </tbody>
+                    </table>
+                    @endif
                 </table>
             </div>
         </div>
